dd9609e643ace9fdea5b7a967aa33caa4b3264ba
Nicholas Christopher <nchristopher@tuta.io>
2023-04-05 14:42:03 -0700

    🐛 Fix menu bg filter on Chromium
    - Due to a Chromium bug, nested elements' backdrop filters are not shown if their parent has a backdrop filter.
    - See https://bugs.chromium.org/p/chromium/issues/detail?id=1318678
    - Using the CSS :has() selector, the Navbar's backdrop filter is removed if a child #nav is checked.
    - A transition is applied to prevent a flash without the backdrop.
    - has() is not supported by Firefox, but this bug doesn't occur there anyway.

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Navbar() {
 	return (
-		<nav className="fixed left-0 top-0 z-50 flex w-full items-center justify-between rounded-b-2xl bg-slate-50/70 px-6 py-4 backdrop-blur-lg dark:bg-slate-950/70 lg:hidden">
+		<nav className="fixed left-0 top-0 z-50 flex w-full items-center justify-between rounded-b-2xl bg-slate-50/70 px-6 py-4 backdrop-blur-lg ease-in-out dark:bg-slate-950/70 lg:hidden [&:has(#nav:checked)]:backdrop-filter-none [&:has(#nav:checked)]:transition">
 			<MobileNav />
 
 			<Link className="text-3xl font-semibold" href="/">
