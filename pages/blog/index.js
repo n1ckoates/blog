7fc119f421709471d8afef9ff16f310df109d0f1
Nick Oates <nick@nickoates.com>
2023-05-13 19:07:17 -0700

    💄 Add images behind links on blog posts page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,13 @@
 import { IconSearch } from "@tabler/icons-react";
-import Head from "next/head";
 import Link from "next/link";
 import { allPosts } from ".contentlayer/generated";
 import { useState } from "react";
 import clsx from "clsx";
+import Image from "next/image";
 
 export default function Blog({ posts }) {
 	const [searchTerm, setSearchTerm] = useState("");
+
 	const filteredPosts = posts.filter((post) => {
 		const searchContent = post.title + post.summary;
 		return searchContent.toLowerCase().includes(searchTerm.toLowerCase());
@@ -38,25 +39,35 @@ export default function Blog({ posts }) {
 			{filteredPosts.map((post) => (
 				<Link
 					href={post.url}
-					key={post._raw.flattenedPath}
-					className="flex flex-col justify-between gap-1 py-4 transition-transform duration-300 ease-in-out hover:scale-105 lg:flex-row"
+					key={post.url}
+					className="group relative mb-4 block overflow-hidden rounded-lg border border-slate-200 drop-shadow dark:border-slate-800 dark:bg-neutral-900/30"
+					aria-label={post.title}
 				>
-					<div className="text-slate-600 dark:text-slate-400 md:text-lg ">
-						<time
-							dateTime={post.date}
-							className="whitespace-pre after:content-['_•_'] lg:after:content-['\A']"
-						>
-							{post.formattedDate}
-						</time>
-						{post.readingTime} min read
-					</div>
-					<div className="w-full lg:w-2/3">
-						<h1 className="mb-1 text-xl font-bold md:text-2xl">
-							{post.title}
-						</h1>
-						<h2 className="text-lg text-slate-600 dark:text-slate-400 md:text-xl">
-							{post.summary}
-						</h2>
+					<Image
+						alt={post.coverAlt}
+						src={post.cover}
+						fill
+						className="absolute -z-10 object-cover brightness-50 transition ease-in-out group-hover:scale-105"
+					/>
+
+					<div className="flex flex-col justify-between gap-1 bg-gradient-to-b from-transparent to-slate-950 p-4 text-white transition-transform duration-300 ease-in-out lg:flex-row">
+						<div className="text-slate-300 md:text-lg ">
+							<time
+								dateTime={post.date}
+								className="whitespace-pre after:content-['_•_'] lg:after:content-['\A']"
+							>
+								{post.formattedDate}
+							</time>
+							{post.readingTime} min read
+						</div>
+						<div className="w-full lg:w-2/3">
+							<h1 className="mb-1 text-xl font-bold md:text-2xl">
+								{post.title}
+							</h1>
+							<h2 className="text-lg text-slate-300 md:text-xl">
+								{post.summary}
+							</h2>
+						</div>
 					</div>
 				</Link>
 			))}
@@ -65,9 +76,17 @@ export default function Blog({ posts }) {
 }
 
 export function getStaticProps() {
-	const posts = allPosts.sort((a, b) =>
-		a.date > b.date ? -1 : a.date < b.date ? 1 : 0
-	);
+	const posts = allPosts
+		.sort((a, b) => (a.date > b.date ? -1 : a.date < b.date ? 1 : 0))
+		.map((post) => ({
+			url: post.url,
+			cover: post.cover,
+			coverAlt: post.coverAlt,
+			formattedDate: post.formattedDate,
+			readingTime: post.readingTime,
+			title: post.title,
+			summary: post.summary,
+		}));
 	return {
 		props: {
 			posts,
