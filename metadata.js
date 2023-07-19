5d31837c1d2f0ce32145442e8c96159f80a3f30c
Nick Oates <nick@nickoates.com>
2023-07-19 00:23:04 -0700

    ♻️ Simplify handling of deployment url

--- metadata.js
+++ metadata.js
@@ -97,3 +97,17 @@ export const projects = {
 		},
 	},
 };
+
+function getURL() {
+	if (process.env.NEXT_PUBLIC_URL) {
+		return process.env.NEXT_PUBLIC_URL;
+	}
+
+	if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+		return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+	}
+
+	return "http://localhost:3000";
+}
+
+export const url = getURL();
