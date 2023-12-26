892941bffbdafa77c4df4c45a1cfb5d2c4d6a9c4
Nick Oates <nick@nickoates.com>
2023-12-25 20:54:54 -0800

    ✨ 404 page improvements
    - Added button to get a new cat
    - Redesigned buttons

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import Link from "next/link";
-import { IconArrowRight, IconLoader2 } from "@tabler/icons-react";
+import { IconCat, IconHome, IconLoader2 } from "@tabler/icons-react";
 import Image from "next/image";
+import { useState } from "react";
+import clsx from "clsx";
 
 export const metadata = {
 	title: "404: That page doesn't exist",
 };
 
 export default function NotFound() {
+	const UNSPLASH_URL =
+		"https://source.unsplash.com/collection/4870076/800x600?t=";
+
+	const [src, setSrc] = useState(UNSPLASH_URL);
+	const [loading, setLoading] = useState(true);
+
+	function handleClick() {
+		setLoading(true);
+
+		setSrc(UNSPLASH_URL + Date.now()); // Force a new image to be loaded
+	}
+
 	return (
 		<div className="text-lg md:text-xl">
 			<h1 className="mb-2 bg-gradient-to-b from-red-600 to-red-700 bg-clip-text text-4xl font-extrabold text-transparent dark:from-red-400 dark:to-red-300 md:h-14 md:text-5xl">
@@ -14,28 +30,41 @@ export default function NotFound() {
 			</h1>
 			<p>To make up for it, here&apos;s a picture of a cat.</p>
 
-			<div className="relative mx-auto mt-6 w-fit overflow-hidden rounded-md bg-zinc-200 shadow-md dark:bg-zinc-800">
-				<IconLoader2
-					className="absolute inset-0 m-auto animate-spin"
-					size={48}
-				/>
+			<div className="relative mx-auto mt-6 w-fit overflow-hidden rounded-2xl border-zinc-400/50 bg-zinc-200 drop-shadow-sm dark:bg-zinc-800">
+				{loading && (
+					<IconLoader2
+						className="absolute inset-0 m-auto animate-spin"
+						size={48}
+					/>
+				)}
 
 				<Image
-					src="https://source.unsplash.com/collection/4870076/800x600"
+					src={src}
 					alt="A cat"
-					className="relative z-10"
+					className={clsx("relative z-10", {
+						"opacity-0": loading && typeof window !== "undefined", // Don't hide image on server, allowing NoScript users to see it
+					})}
 					width={800}
 					height={600}
 					unoptimized
+					onLoad={() => setLoading(false)}
 				/>
 			</div>
 
-			<div className="mt-8 text-center">
-				<Link
-					href="/"
-					className="rounded-md bg-blue-400 px-6 py-4 transition ease-in-out hover:bg-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700"
+			<div className="mx-auto mt-4 flex w-full flex-row justify-center gap-4 md:w-fit">
+				<button
+					className="box-border flex grow cursor-default flex-row items-center justify-center gap-2 rounded-xl border-t border-white/30 bg-gradient-to-b from-zinc-200 to-zinc-400 px-4 py-2 font-semibold drop-shadow-sm active:opacity-70 enabled:hover:from-zinc-100 enabled:hover:to-zinc-300 disabled:opacity-70 dark:from-zinc-700 dark:to-zinc-900 dark:enabled:hover:from-zinc-600 dark:enabled:hover:to-zinc-800"
+					onClick={handleClick}
+					disabled={loading}
 				>
-					Go home <IconArrowRight className="inline-block" />
+					<IconCat />
+					New Cat
+				</button>
+				<Link
+					href={"/"}
+					className="box-border flex grow cursor-default flex-row items-center justify-center gap-2 rounded-xl border-t border-white/30 bg-gradient-to-b from-blue-600 to-blue-800 px-4 py-2 font-semibold text-white drop-shadow-sm hover:from-blue-500 hover:to-blue-700 active:opacity-70 dark:from-blue-700 dark:to-blue-900 dark:hover:from-blue-600 dark:hover:to-blue-800"
+				>
+					<IconHome /> Home
 				</Link>
 			</div>
 		</div>
