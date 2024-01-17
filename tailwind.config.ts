cd495d13bc774395c2c6c23d9e35d74bfe4f7e4f
Nick Oates <nick@nickoates.com>
2024-01-17 08:58:00 -0800

    🏷️ Typesafe Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
+import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
-module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./app/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
+const config: Config = {
+	content: ["./app/**/*.{js,ts,jsx,tsx}"],
 	plugins: [typography],
 	darkMode: "class",
 	future: {
 		hoverOnlyWhenSupported: true,
 	},
 };
+
+export default config;
