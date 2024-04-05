9064d1e89de9dac7cf1597d02b823eebbc118457
Nick Oates <nick@nickoates.com>
2024-04-04 18:04:46 -0700

    🔧 Remove obsolete Next.js config entries
    - `reactStrictMode` is enabled by default in app router.
    - `swcMinify` is enabled by default now.
    - `@tabler/icons-react` is optimized by default now.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
-	reactStrictMode: true,
-	swcMinify: true,
 	images: {
 		deviceSizes: [320, 448, 640, 768, 896],
-	},
-	experimental: {
-		optimizePackageImports: ["@tabler/icons-react"],
 	},
 };
 
