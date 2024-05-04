ed8bc5852e79d9a08f267c5ee9e7713cdb556de8
Nick Oates <nick@nickoates.com>
2024-05-03 17:10:22 -0700

    ✨ Add border and shadow to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -26,8 +26,12 @@ export default function Navbar() {
 	return (
 		<nav
 			className={clsx(
-				"fixed left-0 top-0 z-50 flex w-full items-center justify-between px-6 py-2 before:absolute before:left-0 before:-z-10 before:h-full before:w-full before:rounded-b-2xl before:bg-zinc-50/70 before:backdrop-blur-lg before:transition before:ease-in-out before:dark:bg-zinc-950/70 lg:hidden",
-				{ "before:opacity-100": solid, "before:opacity-0": !solid },
+				"fixed left-0 top-0 z-50 flex w-full items-center justify-between px-6 py-2 before:absolute before:left-0 before:-z-10 before:h-full before:w-full before:rounded-b-2xl before:border-b before:bg-zinc-50/70 before:backdrop-blur-lg before:transition before:ease-in-out before:dark:bg-zinc-950/70 lg:hidden",
+				{
+					"before:border-zinc-200/70 before:opacity-100 before:drop-shadow-lg before:dark:border-zinc-800/70":
+						solid,
+					"before:border-transparent before:opacity-0": !solid,
+				},
 			)}
 		>
 			<Menu open={open} setOpen={setOpen} />
