b5e48fa527ea4f4f934f1b97493a733cb7d644c4
Nick Oates <58091943+n1ckoates@users.noreply.github.com>
2026-07-20 11:59:21 -0700

    🐛 Don't override `type` prop in `<TextInput />`

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -5,8 +5,8 @@ export default function TextInput(
 ) {
 	return (
 		<input
-			{...props}
 			type="text"
+			{...props}
 			className={clsx(
 				"box-border border border-zinc-400/50 bg-white px-4 py-2 placeholder-zinc-500 drop-shadow-xs focus-within:outline-hidden hover:border-zinc-500/50 focus:border-blue-500 dark:border-zinc-600/50 dark:bg-zinc-900 dark:placeholder-zinc-500 dark:hover:border-zinc-500/50 dark:focus:border-blue-500",
 				props.className,
